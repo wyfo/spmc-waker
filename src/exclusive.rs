94d3391fc33f7ff37d35728a5faec158d057efec
Joseph Perez <joperez@hotmail.fr>
2026-03-22 12:14:46 +0100

    Make both thread panic if exclusive check fails

--- src/exclusive.rs
+++ src/exclusive.rs
@@ -1,15 +1,15 @@
-use core::sync::atomic::{AtomicBool, Ordering::Relaxed};
+use core::sync::atomic::{AtomicUsize, Ordering::Relaxed};
 
 #[derive(Debug)]
-pub(crate) struct Exclusive(AtomicBool);
+pub(crate) struct Exclusive(AtomicUsize);
 
 impl Exclusive {
     pub(crate) const fn new() -> Self {
-        Self(AtomicBool::new(false))
+        Self(AtomicUsize::new(0))
     }
 
     pub(crate) fn check(&self) -> ExclusiveGuard<'_> {
-        assert!(!self.0.swap(true, Relaxed), "concurrent access",);
+        assert!(self.0.fetch_add(1, Relaxed) == 0, "concurrent access");
         ExclusiveGuard(self)
     }
 }
@@ -18,6 +18,8 @@ pub(crate) struct ExclusiveGuard<'a>(&'a Exclusive);
 
 impl Drop for ExclusiveGuard<'_> {
     fn drop(&mut self) {
-        self.0.0.store(false, Relaxed);
+        if self.0.0.compare_exchange(1, 0, Relaxed, Relaxed).is_err() {
+            panic!("concurrent access");
+        }
     }
 }
